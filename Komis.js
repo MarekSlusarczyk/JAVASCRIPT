ca4d105a1352a42d55de84503902d7e74cd78259
MarekSlusarczyk <114940674+MarekSlusarczyk@users.noreply.github.com>
2023-12-08 13:38:23 +0100

    obiektowe - cz.2

--- Komis.js
+++ Komis.js
@@ -8,11 +8,33 @@ class Komis {
         this.samochody.push(samochod)
     }
 
-    usunSamochod() {
+    usunSamochod(nazwa, model) {
+        for (let i = 0; i <= this.samochody.length; i++) {
+            console.log(i)
+            if (this.samochody[i].marka == nazwa && this.samochody[i].model == model) {
+                this.samochody.splice(i, 1);
+                break;
+            }
+        }
 
+        console.log(this.samochody)
     }
 
     wyswietlSamochody() {
+        let wynik = '';
+        this.samochody.forEach(samochod => {
+            wynik += `<li>${samochod.marka} ${samochod.model} ${samochod.rocznik} ${samochod.cena} zł</li>
+            `;
+        })
+        return wynik;
+    }
 
+    sprawdzCzyIstnieje(marka, model) {
+        for (let i = 0; i <= this.samochody.length; i++) {
+            if (this.samochody[i].marka == marka && this.samochody[i].model == model) {
+                return true;
+            }
+        }
+        return false;
     }
 }
